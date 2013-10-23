3698ad600bf767509aab89603ab2cd93d55a0edc
Angela Lau <aerlau@gmail.com>
2013-10-23 03:24:44 -0700

    use four spaces for indentation

--- lingo/views.py
+++ lingo/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 def index(request):
-  if request.user.is_authenticated():
-    return redirect('dashboard')
-  return render(request, 'lingo/index.html')
+    if request.user.is_authenticated():
+      return redirect('dashboard')
+    return render(request, 'lingo/index.html')
 
 @login_required
 def dashboard(request):
-  return render(request, 'lingo/dashboard.html')
+    return render(request, 'lingo/dashboard.html')
