d098a3a28bf84ff01f3bea0b9c30456cc14d2a64
Angela Lau <aerlau@gmail.com>
2013-10-23 03:19:15 -0700

    indentation

--- auth/pipeline.py
+++ auth/pipeline.py
@@ -5,13 +5,13 @@ from social.pipeline.partial import partial
 
 @partial
 def require_email(strategy, details, user=None, is_new=False, *args, **kwargs):
-    if user and user.email:
-        return
-    elif is_new and not details.get('email'):
-        if strategy.session_get('saved_email'):
-            details['email'] = strategy.session_pop('saved_email')
-        else:
-            return redirect('require_email')
+  if user and user.email:
+      return
+  elif is_new and not details.get('email'):
+      if strategy.session_get('saved_email'):
+          details['email'] = strategy.session_pop('saved_email')
+      else:
+          return redirect('require_email')
 
 @partial
 def verify_password(strategy, user, is_new=False, *args, **kwargs):
